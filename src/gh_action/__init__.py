676640e345e1845bb7e24da83e86624364aeb6a7
Nikhil Dhandre <ndhandre@redhat.com>
2020-11-23 14:34:16 +0530

    pre-commit job

--- src/gh_action/__init__.py
+++ src/gh_action/__init__.py
@@ -1,14 +1,16 @@
 # Sample just for demo
 
+
 def quickstart():
     return "https://docs.github.com/en/free-pro-team@latest/actions/quickstart"
 
 
 def instroduction():
-    return ("https://docs.github.com/en/free-pro-team@latest/actions/learn-github-actions/"
-            "introduction-to-github-actions")
+    return (
+        "https://docs.github.com/en/free-pro-team@latest/actions/learn-github-actions/"
+        "introduction-to-github-actions"
+    )
 
 
 def own_runners():
     return "https://docs.github.com/en/free-pro-team@latest/actions/hosting-your-own-runners"
-
--- tests/test_gh_demo.py
+++ tests/test_gh_demo.py
@@ -1,13 +1,22 @@
-import gh_action
+from gh_action import instroduction
+from gh_action import own_runners
+from gh_action import quickstart
 
 
 def test_quickstart():
-    assert gh_action.quickstart() == "https://docs.github.com/en/free-pro-team@latest/actions/quickstart"
+    assert quickstart() == "https://docs.github.com/en/free-pro-team@latest/actions/quickstart"
 
 
 def test_own_runners():
-    assert gh_action.own_runners() == "https://docs.github.com/en/free-pro-team@latest/actions/hosting-your-own-runners"
+    assert (
+        own_runners()
+        == "https://docs.github.com/en/free-pro-team@latest/actions/hosting-your-own-runners"
+    )
 
 
 def test_instroduction():
-    assert gh_action.instroduction() == "https://docs.github.com/en/free-pro-team@latest/actions/learn-github-actions/introduction-to-github-actions"
+    assert (
+        instroduction()
+        == "https://docs.github.com/en/free-pro-team@latest/actions/learn-github-actions/"
+        "introduction-to-github-actions"
+    )
